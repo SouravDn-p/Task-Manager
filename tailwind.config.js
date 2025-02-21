22063255a531325a47fdc8e0eccb1b2e0727f87e
Sourav-Dn-p <sdsouravdebnath26@gmail.com>
2025-02-21 09:04:43 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  // eslint-disable-next-line no-undef
+  plugins: [require("daisyui")],
+};
+
